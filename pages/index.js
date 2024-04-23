a2590812dd6952e1f19873e98bf56a02b4615049
Ammon Brown <ammonb@gmail.com>
2024-04-23 12:10:06 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,22 @@ export default function Home() {
       {/* left side */}
       <div className="lg:w-1/2 relative z-10 flex flex-col justify-center px-10 lg:px-20 py-20 lg:py-0 text-left">
         <h2 className="inter text-4xl mb-3 font-bold text-gray-800">
-          Welcome to Your Next.js App{" "}
+          Welcome to HourNexus{" "}
           <span className="block text-blue-500 text-2xl font-normal">
-            Deployed to DigitalOcean
+            The Relationship Intelligence Platform
           </span>
         </h2>
 
         <p className="text-gray-700 mb-6">
-          Deploy API routes, static frontend, databases, and more.
+          Understand who you know, how you know them, and how long you have known them. Automatically!
         </p>
 
         <div className="sm:flex">
           <a
-            href="https://www.digitalocean.com/docs/app-platform"
-            className="block py-2 px-5 rounded shadow bg-gray-500 text-gray-100 sm:mr-2 mb-2 sm:mb-0"
-          >
-            View the Docs
-          </a>
-          <a
-            href="https://cloud.digitalocean.com/apps"
+            href="https://forms.gle/igGEJNwqwFcCjdtD7"
             className="block py-2 px-5 rounded shadow bg-blue-500 text-blue-100"
           >
-            View Your Dashboard
+            Join the Waitlist!
           </a>
         </div>
       </div>
